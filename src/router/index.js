512ef011d2bc5348b21e91cef28d850b14f15fda
kionou <kionoumamadou.00@gmail.com>
2023-06-22 18:33:38 +0000

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -8,25 +8,27 @@ import Offre from '../views/Public/Opportunite/offre.vue'
 import Emploi from '../views/Public/Opportunite/emploi.vue'
 // import Connexion from '../views/Public/Connexion.vue'
 
-
+const routes= [
+  {
+    path: '/',
+    name: 'public',
+    component: LayoutPublic,
+    children:[
+       { path: '/', name: 'accueil', component: Accueil},
+       { path: '/liste_pme', name: 'liste_pme', component: Liste},
+       { path: '/statistique', name: 'statistique', component: Statistique},
+       { path: '/documents', name: 'documents', component: Documents},
+       { path: '/opportunites/appel_offre', name: 'appel_offre', component: Offre},
+       { path: '/opportunites/appel_emploi', name: 'appel_emploi', component: Emploi},
+       { path: '/connexion', name: 'connexion',  component: () => import('../views/Public/Connexion.vue'),}
+    ]
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'public',
-      component: LayoutPublic,
-      children:[
-         { path: '/', name: 'accueil', component: Accueil},
-         { path: '/liste_pme', name: 'liste_pme', component: Liste},
-         { path: '/statistique', name: 'statistique', component: Statistique},
-         { path: '/documents', name: 'documents', component: Documents},
-         { path: '/opportunites/appel_offre', name: 'appel_offre', component: Offre},
-         { path: '/opportunites/appel_emploi', name: 'appel_emploi', component: Emploi},
-         { path: '/connexion', name: 'connexion',  component: () => import('../views/Public/Connexion.vue'),}
-      ]
-    },
-  ]
-})
+  routes,
+});
+
+
 
 export default router
