6750038181266ba3c8ab414083c6db5b585fc5fe
kionou <kionoumamadou.00@gmail.com>
2023-06-22 08:56:03 +0000

    update modules

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,26 @@
 
 </template>
 <script >
-import AOS from "aos/dist/aos"
+  import AOS from "aos/dist/aos"
 // import "@/assets/css/style.css"
-import "@/assets/vendor/bootstrap/css/bootstrap.min.css"
-import "@/assets/vendor/bootstrap-icons/bootstrap-icons.css"
+// import "@/assets/vendor/bootstrap/css/bootstrap.min.css"
+// import "@/assets/vendor/bootstrap-icons/bootstrap-icons.css"
 import "@/assets/vendor/aos/aos.css"
 import "aos/dist/aos.css"
-import "@/assets/vendor/glightbox/css/glightbox.min.css"
-import "@/assets/vendor/swiper/swiper-bundle.min.css"
+// import "@/assets/vendor/glightbox/css/glightbox.min.css"
+// import "@/assets/vendor/swiper/swiper-bundle.min.css"
 import "@/assets/css/main.css"
-import "@/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"
-import "@/assets/vendor/glightbox/js/glightbox.min.js"
+// import "@/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"
+// import "@/assets/vendor/glightbox/js/glightbox.min.js"
 import "@/assets/vendor/purecounter/purecounter_vanilla.js"
-import "@/assets/vendor/swiper/swiper-bundle.min.js"
+// import "@/assets/vendor/swiper/swiper-bundle.min.js"
 //  import  "@/assets/vendor/aos/aos.js"
-import "@/assets/vendor/isotope-layout/isotope.pkgd.min.js"
+
+// import "@/assets/vendor/isotope-layout/isotope.pkgd.min.js"
 import "@/assets/vendor/php-email-form/validate.js"
 import "@/assets/js/main.js"
 // import "@/assets/js/app.js"
+
 
 
   export default {
